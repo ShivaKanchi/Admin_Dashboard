89d5626f18bc15b3c005ffb2e9970021f59a009c
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-16 23:47:38 +0530

    Still working on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,11 +10,33 @@ const Home = () => {
         <div className='mt-12'>
             <div className='flex flex-wrap lg:flex-nowrap justify-center'>
                 <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl  w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'>
+                    <div className='flex justify-between items-center'>
+                        <div>
+                            <p>Earnings</p>
+                            <p>$69,433.21</p>
+                        </div>
+                    </div>
+                    <div className='mt-6 '>
+                        <Button
+                            color='white'
+                            bgColor='blue'
+                            text='Download'
+                            borderRadius='10px'
+                            size='md'
+                        />
+                    </div>
+                </div>
+                <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
+                    {earningData.map((item) => (
+                        <div
+                            key={item.title}
+                            className="bg-white dark:text-gray-200"
+                        >
 
+                        </div>
+                    ))}
                 </div>
             </div>
-
-
         </div>
     )
 }
