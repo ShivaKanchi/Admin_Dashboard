e164bf5be3c247b4bad76681ac002d950dd80db7
ShivaKanchi <shivakanchi111@gmail.com>
2023-03-21 23:05:29 +0530

    Done with few charts and Theme settings

--- src/components/charts/Sparkline.jsx
+++ src/components/charts/Sparkline.jsx
@@ -1,34 +1,35 @@
 import React from 'react'
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts'
+class Sparkline extends React.PureComponent {
+    render() {
+        const { id, height, width, color, data, type, currentColor } = this.props
 
-const Sparkline = ({ id, height, width, color, data, type, currentColor }) => {
-    return (
-        <SparklineComponent
-            id={id}
-            height={height}
-            width={width}
-            LineWidth={1}
-            valueType="Numeric"
-            fill={color}
-            border={{ color: currentColor, width: 2 }}
-            dataSource={data}
-            xName="x"
-            yName="yval"
-            type={type}
-            tooltipSettings={{
-                visible: true,
-                format: '${x}: Data ${yval}',
-                trackLineSettings: {
-                    visible: true
-                }
-            }}
-        >
-            <Inject services={[SparklineTooltip]} />
-        </SparklineComponent>
-    );
-
+        return (
+            <SparklineComponent
+                id={id}
+                height={height}
+                width={width}
+                LineWidth={1}
+                valueType="Numeric"
+                fill={color}
+                border={{ color: currentColor, width: 2 }}
+                dataSource={data}
+                xName="x"
+                yName="yval"
+                type={type}
+                tooltipSettings={{
+                    visible: true,
+                    format: '${x}: Data ${yval}',
+                    trackLineSettings: {
+                        visible: true
+                    }
+                }}
+            >
+                <Inject services={[SparklineTooltip]} />
+            </SparklineComponent>
+        );
+    }
 }
-
 export default Sparkline
 
 
